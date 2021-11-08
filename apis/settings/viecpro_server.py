3a94ca32d8a338d1ede105158a9690f1e586ea73
newmountainprogramming <pirgie@gmx.net>
2021-11-08 16:59:53 +0100

    included viecpro_vis-app for the apis viecpro-instance

--- apis/settings/viecpro_server.py
+++ apis/settings/viecpro_server.py
@@ -45,6 +45,8 @@ DEV_VERSION = False
 SPECTACULAR_SETTINGS["COMPONENT_SPLIT_REQUEST"] = True
 SPECTACULAR_SETTINGS["COMPONENT_NO_READ_ONLY_REQUIRED"] = True
 
+
+INSTALLED_APPS += ["viecpro_vis"]
 DATABASES = {}
 
 DATABASES["default"] = dj_database_url.config(conn_max_age=600)
