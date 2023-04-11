e50b47ef3795f10e6d1c1b57fd2f128196b0ba65
Gregor Pirgie <78487560+gregorpirgie@users.noreply.github.com>
2023-04-11 17:49:40 +0200

    Adds Bibsonomy Secrets to VieCPro

--- apis/settings/viecpro_server.py
+++ apis/settings/viecpro_server.py
@@ -126,3 +126,11 @@ ROBOTS_TXT_FOLDER = os.path.join(BASE_DIR, "robots_template")
 
 # register above folder as a template-dir
 TEMPLATES[0]["DIRS"] += [ROBOTS_TXT_FOLDER,]
+
+APIS_BIBSONOMY = [{     
+   'type': 'zotero', 
+   'url': 'https://api.zotero.org/',
+   'user': os.environ.get('APIS_BIBSONOMY_USER', ' '),
+   'API key': os.environ.get('APIS_BIBSONOMY_API_KEY', ' '), 
+   'group':  os.environ.get('APIS_BIBSONOMY_GROUP', ' ')
+}]
