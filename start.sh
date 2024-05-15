c56541e58ffaf68efe3be158a74718a25671da47
Birger Schacht <birger.schacht@oeaw.ac.at>
2024-05-15 08:59:05 +0200

    fix: make gunicorn verbose

--- start.sh
+++ start.sh
@@ -8,5 +8,5 @@ python manage.py collectstatic --noinput
 #python manage.py build_solr_schema --configure-directory /var/solr_new/paas_solr/conf --reload-core default
 if [[ -z "${DEVELOP}" ]]; then
     echo "starting gunicorn"
-    gunicorn apis.wsgi -b 0.0.0.0:5000 --timeout 120 --workers=3 --threads=3 --worker-connections=1000
+    gunicorn apis.wsgi -b 0.0.0.0:5000 --timeout 120 --workers=3 --threads=3 --worker-connections=1000 --access-logfile - --error-logfile -
 fi
